9b43761aa79b357ca99c060a710652dcddf74a7f
Eric Chew <csm012009@coderacademy.edu.au>
2020-07-15 04:42:26 +0000

    Added scoring (speed) framework

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import random
 import pangrams
 
 # statics & data structures
-APPNAME = 'ICBIN Terminal'
-GAMELENGTH = 3
+APPNAME = 'ICBIN TTT'
+GAMELENGTH = 1
+COUNTDOWN_TIMER = 3
+COUNTDOWN_MSG = 'GO!'
 
 MENU_ALIASES = {
     'quickplay' : 'quickplay',
+    'q' : 'quickplay', # consider removing
     'help' : 'help',
     'exit' : 'app_exit'
 }
@@ -17,14 +20,30 @@ MENU_ALIASES = {
 def welcome_msg():
     print(f'Welcome to {APPNAME}!')
 
+def countdown(timer):
+    for sec in range(timer, 0, -1):
+        print(sec)
+        time.sleep(1)
+    print(COUNTDOWN_MSG)
+
 def quickplay():
     print('quickplay activated')
+    char_count = 0
+    start = time.time()
     for sentence in random.sample(pangrams.pangrams, GAMELENGTH):
+        char_count += len(sentence)
         user_type = input(f'Sentence is:\n{sentence}\n')
         if user_type == sentence:
             print('Nice')
         else:
             print('Aww, sad')
+    end = time.time()
+    total_time_sec = end - start
+    total_time_min = total_time_sec / 60
+    cpm = char_count / total_time_min
+    wpm = cpm / 5
+    print(f'Your CPM (characters per minute) is {cpm:.02f}!')
+    print(f'Your WPM (words per minute) is {wpm:.02f}!')
 
 def help():
     print('printing help message')
--- pangrams.py
+++ pangrams.py
@@ -2,5 +2,17 @@
 pangrams = [
     'Jim quickly realized that the beautiful gowns are expensive',
     'Jackie will budget for the most expensive zoology equipment',
-    'Zack Gappow saved the job requirement list for the six boys'
+    'Zelda might fix the job growth plans very quickly on Monday',
+    'The wizard quickly jinxed the gnomes before they vaporized',
+    'All questions asked by five watched experts amaze the judge',
+    'A quick movement of the enemy will jeopardize six gunboats',
+    'How razorback-jumping frogs can level six piqued gymnasts',
+    'Just keep examining every low bid quoted for zinc etchings',
+    'Back in June we delivered oxygen equipment of the same size',
+    'Grumpy wizards make toxic brew for the evil queen and jack',
+    'A quivering Texas zombie fought republic linked jewelry',
+    'Crazy Frederick bought many very exquisite opal jewels',
+    'Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!',
+    'Just poets wax boldly as kings and queens march over fuzz',
+    'Few black taxis drive up major roads on quiet hazy nights'
 ]
