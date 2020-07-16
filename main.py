14714cf322045c7dfbd3a268d94057cd613e16ee
Eric Chew <csm012009@coderacademy.edu.au>
2020-07-16 05:54:01 +0000

    Improved accuracy scoring, "loading"

--- main.py
+++ main.py
@@ -36,14 +36,15 @@ def longer_len(str1, str2):
 
 def quickplay():
     print('quickplay activated')
-    sess_char_count = 0
     total_time_sec = 0
-    len_total = 0
+    total_correct_chars = 0
+    total_incorrect_chars = 0
     for sentence in random.sample(pangrams.pangrams, GAMELENGTH):
         countdown(COUNTDOWN_TIMER)
         seg_start = time.time()
         sentence_length = len(sentence)
         sentence_correct_chars = 0
+        sentence_incorrect_chars = 0
         user_type = input(f'Sentence is:\n{sentence}\n')
         seg_end = time.time()
         seg_time = seg_end - seg_start
@@ -51,22 +52,26 @@ def quickplay():
             try:
                 if char == user_type[index]:
                     sentence_correct_chars += 1
+                else:
+                    sentence_incorrect_chars += 1
             except:
-                pass
-        sentence_accuracy = sentence_correct_chars / longer_len(sentence, user_type)
+                break
+        if sentence_length > len(user_type):
+            sentence_incorrect_chars += sentence_length - len(user_type)
+        sentence_accuracy = sentence_correct_chars / (sentence_correct_chars + sentence_incorrect_chars)
         print(f'This sentence was typed with {(sentence_accuracy * 100):.02f}% accuracy!')
-        sess_char_count += sentence_correct_chars
         total_time_sec += seg_time
-        len_total += longer_len(sentence, user_type)
+        total_correct_chars += sentence_correct_chars
+        total_incorrect_chars += sentence_incorrect_chars
     total_time_min = total_time_sec / 60
-    cpm = sess_char_count / total_time_min
+    cpm = total_correct_chars / total_time_min
     wpm = cpm / 5
-    overall_accuracy = sess_char_count / len_total
-    print('Great work! Tabulating results', end = '')
+    overall_accuracy = total_correct_chars / (total_correct_chars + total_incorrect_chars)
+    print('Great work! Tabulating results', end = '', flush = True)
     for i in range(0, COUNTDOWN_TIMER):
-        print('.', end='')
+        print('.', end='', flush = True)
         time.sleep(1)
-    print(f'Your CPM (characters per minute) is {cpm:.02f}!')
+    print(f'\nYour CPM (characters per minute) is {cpm:.02f}!')
     print(f'Your WPM (words per minute) is {wpm:.02f}!')
     print(f'Your overall accuracy was {(overall_accuracy * 100):.02f}%!')
 
