a05e79968b8eb0d1f317a11b0dd24b9a96c18fea
Eric Chew <csm012009@coderacademy.edu.au>
2020-07-16 04:46:14 +0000

    Implemented countdown and base accuracy scoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pangrams
 
 # statics & data structures
 APPNAME = 'ICBIN TTT'
-GAMELENGTH = 1
+GAMELENGTH = 3
 COUNTDOWN_TIMER = 3
 COUNTDOWN_MSG = 'GO!'
 
@@ -21,29 +21,54 @@ def welcome_msg():
     print(f'Welcome to {APPNAME}!')
 
 def countdown(timer):
+    print(f'Get ready for the next sentence in {timer} seconds!')
+    time.sleep(1)
     for sec in range(timer, 0, -1):
         print(sec)
         time.sleep(1)
     print(COUNTDOWN_MSG)
 
+def longer_len(str1, str2):
+    if len(str1) >= len(str2):
+        return len(str1)
+    else:
+        return len(str2)
+
 def quickplay():
     print('quickplay activated')
-    char_count = 0
-    start = time.time()
+    sess_char_count = 0
+    total_time_sec = 0
+    len_total = 0
     for sentence in random.sample(pangrams.pangrams, GAMELENGTH):
-        char_count += len(sentence)
+        countdown(COUNTDOWN_TIMER)
+        seg_start = time.time()
+        sentence_length = len(sentence)
+        sentence_correct_chars = 0
         user_type = input(f'Sentence is:\n{sentence}\n')
-        if user_type == sentence:
-            print('Nice')
-        else:
-            print('Aww, sad')
-    end = time.time()
-    total_time_sec = end - start
+        seg_end = time.time()
+        seg_time = seg_end - seg_start
+        for index, char in enumerate(sentence):
+            try:
+                if char == user_type[index]:
+                    sentence_correct_chars += 1
+            except:
+                pass
+        sentence_accuracy = sentence_correct_chars / longer_len(sentence, user_type)
+        print(f'This sentence was typed with {(sentence_accuracy * 100):.02f}% accuracy!')
+        sess_char_count += sentence_correct_chars
+        total_time_sec += seg_time
+        len_total += longer_len(sentence, user_type)
     total_time_min = total_time_sec / 60
-    cpm = char_count / total_time_min
+    cpm = sess_char_count / total_time_min
     wpm = cpm / 5
+    overall_accuracy = sess_char_count / len_total
+    print('Great work! Tabulating results', end = '')
+    for i in range(0, COUNTDOWN_TIMER):
+        print('.', end='')
+        time.sleep(1)
     print(f'Your CPM (characters per minute) is {cpm:.02f}!')
     print(f'Your WPM (words per minute) is {wpm:.02f}!')
+    print(f'Your overall accuracy was {(overall_accuracy * 100):.02f}%!')
 
 def help():
     print('printing help message')
@@ -52,9 +77,9 @@ def app_exit():
     print('Thanks for playing!')
     exit()
 
-# main program
+# main program flow
 welcome_msg()
-continue_playing = True
+# continue_playing = True
 while True:
     user_in = input('Input Command: ')
     if user_in in MENU_ALIASES:
--- pangrams.py
+++ pangrams.py
@@ -1,4 +1,5 @@
 # Pangrams sourced from http://clagnut.com/blog/2380/
+
 pangrams = [
     'Jim quickly realized that the beautiful gowns are expensive',
     'Jackie will budget for the most expensive zoology equipment',
