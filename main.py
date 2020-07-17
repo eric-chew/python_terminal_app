b2f06ebbd2038fef43692e77f2d7043de506f695
Eric Chew <csm012009@coderacademy.edu.au>
2020-07-17 11:46:44 +0000

    Enhanced messages and menus

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 import time
 import random
 import pangrams
+from os import system
 from termcolor import colored, cprint
 
 # statics & data structures
-APPNAME = 'ICBIN TTT'
+APPNAME = 'Terminal Typing Test'
 GAMELENGTH = 3
 COUNTDOWN_TIMER = 3
 COUNTDOWN_MSG = 'GO!'
@@ -17,13 +18,28 @@ CPW = 5 # Characters per word
 TIMER_DELAY = 1
 
 MENU_ALIASES = {
+    '1' : 'quickplay',
     'quickplay' : 'quickplay',
-    'q' : 'quickplay', # consider removing
+    '3' : 'help',
     'help' : 'help',
-    'exit' : 'app_exit'
+    '4' : 'app_exit',
+    'exit' : 'app_exit',
 }
 
-EOL_PUNCT = {'.', '!', '?'}
+EOG_ALIASES = {
+    '1' : True,
+    '1. play again' : True,
+    'play' : True,
+    'play again': True,
+    'again' : True,
+    '2' : False,
+    '2. return to menu' : False,
+    'return' : False,
+    'return to menu' : False,
+    'menu' : False
+}
+
+EOL_PUNCT = {'.', '!', '?', '\n'}
 
 # function definitions
 def tprint(str):
@@ -67,7 +83,7 @@ def tprint_float(str):
     print('')
 
 def welcome_msg():
-    tprint(f'Welcome to {APPNAME}!')
+    tprint(f'Welcome! My name is {APPNAME}!')
 
 def countdown(timer):
     tprint(f'Get ready for the next sentence in {timer} seconds!')
@@ -84,49 +100,70 @@ def longer_len(str1, str2):
         return len(str2)
 
 def quickplay():
+    play = True
     tprint('quickplay activated.')
-    total_time_sec = 0
-    total_correct_chars = 0
-    total_incorrect_chars = 0
-    try:
-        sess_sentences = random.sample(pangrams.pangrams_test, GAMELENGTH)
-    except:
-        sess_sentences = random.sample(pangrams.pangrams_test, len(pangrams.pangrams_test))
-    sess_len = len(sess_sentences)
-    for sindex, sentence in enumerate(sess_sentences, 1):
-        countdown(COUNTDOWN_TIMER)
-        seg_start = time.time()
-        cprint(sentence, 'white', 'on_grey')
-        sentence_length = len(sentence)
-        sentence_correct_chars = 0
-        sentence_incorrect_chars = 0
-        user_type = input('')
-        seg_end = time.time()
-        seg_time = seg_end - seg_start
-        for cindex, char in enumerate(sentence):
-            try:
-                if char == user_type[cindex]:
-                    sentence_correct_chars += 1
-                else:
+    while play:
+        total_time_sec = 0
+        total_correct_chars = 0
+        total_incorrect_chars = 0
+        try:
+            sess_sentences = random.sample(pangrams.pangrams_test, GAMELENGTH)
+        except:
+            sess_sentences = random.sample(pangrams.pangrams_test, len(pangrams.pangrams_test))
+        sess_len = len(sess_sentences)
+        for sindex, sentence in enumerate(sess_sentences, 1):
+            if sindex < sess_len:
+                tinput(f'Press ENTER when ready')
+            countdown(COUNTDOWN_TIMER)
+            seg_start = time.time()
+            cprint(sentence, 'white', 'on_grey')
+            sentence_length = len(sentence)
+            sentence_correct_chars = 0
+            sentence_incorrect_chars = 0
+            user_type = input('')
+            seg_end = time.time()
+            seg_time = seg_end - seg_start
+            for cindex, char in enumerate(sentence):
+                try:
+                    if char == user_type[cindex]:
+                        sentence_correct_chars += 1
+                    else:
+                        sentence_incorrect_chars += 1
+                except:
                     sentence_incorrect_chars += 1
+            if sentence_length < len(user_type):
+                sentence_incorrect_chars += len(user_type) - sentence_length
+            sentence_accuracy = sentence_correct_chars / (sentence_correct_chars + sentence_incorrect_chars)
+            tprint_float(f'This sentence was typed with {(sentence_accuracy * 100):.02f}% accuracy!')
+            total_time_sec += seg_time
+            total_correct_chars += sentence_correct_chars
+            total_incorrect_chars += sentence_incorrect_chars
+        total_time_min = total_time_sec / 60
+        cpm = total_correct_chars / total_time_min
+        wpm = cpm / CPW
+        overall_accuracy = total_correct_chars / (total_correct_chars + total_incorrect_chars)
+        thinking('Great work! Calculating Performance', COUNTDOWN_TIMER)
+        tprint_float(f'Your typing speed is {wpm:.02f}WPM (words per minute)!')
+        tprint_float(f'Your overall accuracy was {(overall_accuracy * 100):.02f}%!')
+        while True:
+            next = tinput('''Would you like to:
+1. Play Again
+2. Return to Main Menu
+''').lower().strip()
+            try:
+                play = EOG_ALIASES[next]
+                break
             except:
-                sentence_incorrect_chars += 1
-        if sentence_length < len(user_type):
-            sentence_incorrect_chars += len(user_type) - sentence_length
-        sentence_accuracy = sentence_correct_chars / (sentence_correct_chars + sentence_incorrect_chars)
-        tprint_float(f'This sentence was typed with {(sentence_accuracy * 100):.02f}% accuracy!')
-        total_time_sec += seg_time
-        total_correct_chars += sentence_correct_chars
-        total_incorrect_chars += sentence_incorrect_chars
-        if sindex < sess_len:
-            tinput(f'Press ENTER to continue')
-    total_time_min = total_time_sec / 60
-    cpm = total_correct_chars / total_time_min
-    wpm = cpm / CPW
-    overall_accuracy = total_correct_chars / (total_correct_chars + total_incorrect_chars)
-    thinking('Great work! Calculating Performance', COUNTDOWN_TIMER)
-    tprint_float(f'Your typing speed is {wpm:.02f}WPM (words per minute)!')
-    tprint_float(f'Your overall accuracy was {(overall_accuracy * 100):.02f}%!')
+                tprint('Sorry, I didn\'t understand. Could you rephrase that?')
+
+def mm_msg():
+    tprint(
+f'''Reminder that your available commands are:
+1. Quickplay: A quick test using from a pre-defined list of sentences
+2. TBI
+3. Help: An explanation of the different menu options
+4. Exit: Exits program
+''')
 
 def help():
     tprint('printing help message.')
@@ -137,9 +174,9 @@ def app_exit():
 
 # main program flow
 welcome_msg()
-# continue_playing = True
 while True:
-    user_in = tinput('Input Command: ')
+    mm_msg()
+    user_in = tinput('Input Command: ').lower().strip()
     if user_in in MENU_ALIASES:
         eval(f'{MENU_ALIASES[user_in]}()')
     else:
--- pangrams.py
+++ pangrams.py
@@ -15,7 +15,21 @@ pangrams = [
     'Crazy Frederick bought many very exquisite opal jewels',
     'Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!',
     'Just poets wax boldly as kings and queens march over fuzz',
-    'Few black taxis drive up major roads on quiet hazy nights'
+    'Few black taxis drive up major roads on quiet hazy nights',
+    'Jack quietly moved up front and seized the big ball of wax',
+    'A very big box sailed up then whizzed quickly from Japan',
+    'Cute, kind, jovial, foxy physique, amazing beauty? Wowser!',
+    'Big July earthquakes confound zany experimental vow',
+    'Six big devils from Japan quickly forgot how to waltz',
+    'Then a cop quizzed Mick Jagger’s ex-wives briefly',
+    'My girl wove six dozen plaid jackets before she quit',
+    'As quirky joke, chefs won’t pay devil magic zebra tax',
+    'Painful zombies quickly watch a jinxed graveyard',
+    'Foxy diva Jennifer Lopez wasn’t baking my quiche',
+    'The lazy major was fixing Cupid\'s broken quiver',
+    'The quick onyx goblin jumps over the lazy dwarf',
+    'Who packed five dozen old quart jugs in my box?',
+    'This Pangram contains four a\'s, one b, two c\'s, one d, thirty e\'s, six f\'s, five g\'s, seven h\'s, eleven i\'s, one j, one k, two l\'s, two m\'s, eighteen n\'s, fifteen o\'s, two p\'s, one q, five r\'s, twenty-seven s\'s, eighteen t\'s, two u\'s, seven v\'s, eight w\'s, two x\'s, three y\'s, & one z'
 ]
 
 pangrams_test = ['123',
